44acec8160ce14191a6ef76d8d05b337f5075fa4
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-12-06 23:46:02 +0100

    Combine two inputs into an object and update inpute enteries by managing complex states.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,42 @@
 import React, { useState } from "react";
 
 function App() {
-  const [fName, setFName] = useState("");
-  const[lName, setLname] = useState("");
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: ""
+  });
 
-  function handleFName(event) {
-    setFName(event.target.value);
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    const inputName = event.target.name;
+
+     setFullName((preValue)=> {
+       
+      if(inputName === "fName") {
+        return {
+          fName: newValue,
+          lName: preValue.lName
+        }
+      } else if (inputName === "lName") {
+        return {
+          fName: preValue.fName,
+          lName: newValue
+        }
+      }
+
+     });
+   
   }
 
-  function handleLName(event) {
-    setLname(event.target.value);
-  }
+
 
   return (
     <div className="container">
-      <h1>Hello {fName} {lName} </h1>
+      <h1>Hello {fullName.fName} {fullName.lName} </h1>
       <form>
-        <input name="fName" value={fName} onChange={handleFName} placeholder="First Name" />
-        <input name="lName" value={lName} onChange={handleLName} placeholder="Last Name" />
+        <input name="fName" value={fullName.fName} onChange={handleChange} placeholder="First Name" />
+        <input name="lName" value={fullName.lName} onChange={handleChange} placeholder="Last Name" />
         <button>Submit</button>
       </form>
     </div>
