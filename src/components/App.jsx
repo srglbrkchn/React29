acf925aa710cf878520e08ffa00c4d38d884a959
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-12-06 22:47:58 +0100

    Use useState function and 2 different handleChange to handle change of 2 different inputs.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fName, setFName] = useState("");
+  const[lName, setLname] = useState("");
+
+  function handleFName(event) {
+    setFName(event.target.value);
+  }
+
+  function handleLName(event) {
+    setLname(event.target.value);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>Hello {fName} {lName} </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input name="fName" value={fName} onChange={handleFName} placeholder="First Name" />
+        <input name="lName" value={lName} onChange={handleLName} placeholder="Last Name" />
         <button>Submit</button>
       </form>
     </div>
